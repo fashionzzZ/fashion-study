672809881c7f3cbac3fd223ab9a83a8fca6c6499
Fashion <1006262073@qq.com>
2020-08-28 13:47:20 +0800

    日常验证代码

--- fashion-juc/src/main/java/com/fashion/juc/Test.java
+++ fashion-juc/src/main/java/com/fashion/juc/Test.java
@@ -8,6 +8,11 @@ package com.fashion.juc;
  **/
 public class Test {
     public static void main(String[] args) {
-
+        String s1 = "programming";
+        String s2 = "ming";
+        String s3 = "program" + s2;
+        String s4 = "program" + "ming";
+        System.out.println(s1 == s3);
+        System.out.println(s1 == s4);
     }
 }
--- fashion-redis/src/main/java/com/fashion/redis/entity/Product.java
+++ fashion-redis/src/main/java/com/fashion/redis/entity/Product.java
@@ -1,13 +1,16 @@
 package com.fashion.redis.entity;
 
+import java.io.Serializable;
+
 /**
  * 商品实体类
  *
  * @author Wuxf
  * @date 2020/08/20
  **/
-public class Product {
+public class Product implements Serializable {
 
+    private static final long serialVersionUID = -4153116047316155120L;
     /**
      * 商品ID
      */
